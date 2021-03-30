05bcb4cf3b3b347dcc6d3368b1dd1289964a1f25
Adrian Furman <fadikk367@gmail.com>
2021-03-30 22:30:58 +0200

    Do initial project cleanup

---
+++ src/App.css.tsx
@@ -0,0 +1,13 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+`;
+
+export const Layout = styled.div`
+  min-height: 100vh;
+`;
