77e87c91b50052b1bd4abd0c5178d43cbe1e3335
HappyButter <mikolaj.marchewa@gmail.com>
2021-05-04 22:04:00 +0200

    outline color fix

--- src/components/FormikField/formikField-styles.tsx
+++ src/components/FormikField/formikField-styles.tsx
@@ -6,6 +6,17 @@ export const CustomTextField = styled(TextField)`
 		width: 400px;
 		margin: 7px 0;
 		border-radius: 0;
+		border-color: rgb(256, 156, 44);
+
+		& label.Mui-focused {
+			color: rgb(256, 156, 44);
+		}
+
+		& .MuiOutlinedInput-root {
+			&.Mui-focused fieldset {
+				border-color: rgb(256, 156, 44);
+			}
+		}
 
 		@media (max-width: 800px) {
 			width: 90%;
