9924813823059889787e8701825d8d16c604d511
Adrian Furman <fadikk367@gmail.com>
2021-05-13 00:43:09 +0200

    Restructure views to distinguish Landing and Home pages

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,25 +1,12 @@
 import React from "react";
 
 import { ContentWrapper } from "styles/ContentWrapper";
-import { OrangeButton } from "styles/OrangeButton";
-import { Link } from "styles/Link";
+
 
 const Home: React.FC = () => {
 	return (
 		<ContentWrapper>
-			<h1>Studentify</h1>
-			<br />
-			<br />
-			<br />
-			<OrangeButton component={Link} to="/register">
-				<h3>Let's get started!</h3>
-			</OrangeButton>
-			<br />
-			<br />
-			<br />
-			<h3>
-				Already have an account? <Link to="/login">Login!</Link>
-			</h3>
+			<h1>Home</h1>
 		</ContentWrapper>
 	);
 };
