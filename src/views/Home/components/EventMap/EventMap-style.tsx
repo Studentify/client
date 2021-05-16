15ec0312a8d51b8e04f781ff94eb662b1612da4d
Adrian Furman <fadikk367@gmail.com>
2021-05-16 15:06:15 +0200

    Create basic map to display

--- src/views/Home/components/EventMap/EventMap-style.tsx
+++ src/views/Home/components/EventMap/EventMap-style.tsx
@@ -7,3 +7,35 @@ export const MapWrapper = styled.div`
   justify-content: center;
   align-items: center;
 `;
+export const MapElement = styled.div`
+  width: 100%;
+  height: 100%;
+
+  & {
+    .ol-zoom {
+      position: absolute;
+      right: 0;
+      display: flex;
+      flex-direction: column;
+      margin: 0.5rem;
+      gap: 2px;
+    }
+
+    .ol-rotate {
+      display: none;
+    }
+
+    button {
+      width: 30px;
+      height: 30px;
+      background-color: rgba(63, 81, 181, 0.9);
+      color: white;
+      font-weight: 700;
+      font-size: 16px;
+      border: none;
+      outline: none;
+      border-radius: 5px;
+      cursor: pointer;
+    }
+  }
+`;
--- src/views/Home/components/EventMap/EventMap.tsx
+++ src/views/Home/components/EventMap/EventMap.tsx
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-import { MapWrapper } from "./EventMap-style";
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import { fromLonLat } from 'ol/proj';
+import XYZ from 'ol/source/XYZ';
+
+import { MapWrapper, MapElement } from "./EventMap-style";
 
 
 const EventMap = () => {
+  const [map, setMap] = useState<Map>(new Map({
+    layers: [
+      new TileLayer({
+        source: new XYZ({
+          url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        })
+      })
+    ],
+    view: new View({
+      center: fromLonLat([19.916, 50.067]),
+      zoom: 15
+    })
+  }));
+
+  useEffect(() => {
+    map.setTarget("map");
+  }, [])
+
   return (
     <MapWrapper>
-      There will be the map
+      <MapElement id="map"></MapElement>
     </MapWrapper>
   )
 }
