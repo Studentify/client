f248aafa147aeb8d67cce48801af3203ed10cbd6
Adrian Furman <fadikk367@gmail.com>
2021-05-16 14:14:16 +0200

    Fix localStorage key for auth token to "token"

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -5,7 +5,7 @@ const baseURL = 'https://localhost:44365/api';
 const instance = axios.create({ baseURL });
 
 instance.interceptors.request.use(function (config) {
-  const token = localStorage.getItem('authToken');
+  const token = localStorage.getItem('token');
 
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
