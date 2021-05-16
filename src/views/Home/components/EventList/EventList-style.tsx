fe1449fc04999ae4fb0a10a4447c30ebb175d271
Adrian Furman <fadikk367@gmail.com>
2021-05-16 14:13:19 +0200

    Add basic structure for EventList

--- src/views/Home/components/EventList/EventList-style.tsx
+++ src/views/Home/components/EventList/EventList-style.tsx
@@ -1,1 +1,44 @@
-export {};
+import styled from "styled-components";
+
+
+export const List = styled.ul`
+  list-style-type: none;
+  position: relative;
+  height: 100%;
+`;
+
+export const EventsHeader = styled.header`
+  display: flex;
+  padding: 1rem;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const EventItem = styled.li`
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  background-color: #ffffff;
+  transition: background-color 0.2s ease-in-out;
+
+  &:not(:last-child) {
+    margin-bottom: 0.25rem;
+  }
+
+  &:hover {
+    background-color: #ececec;
+    cursor: pointer;
+  }
+`;
+
+export const EventHeader = styled.h4`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const EventDate = styled.span`
+  color: gray;
+  font-size: 0.8rem;
+  font-weight: 300;
+`;
--- src/views/Home/components/EventList/EventList.tsx
+++ src/views/Home/components/EventList/EventList.tsx
@@ -1,13 +1,52 @@
 import React from "react";
 
+import Button from "@material-ui/core/Button";
+import FilterListIcon from "@material-ui/icons/FilterList";
 
-const EventList = () => {
+import { List, EventsHeader, EventItem, EventHeader, EventDate } from "./EventList-style";
+
+interface Event {
+	id: number;
+	eventType: string;
+	name: string;
+	creationDate: string;
+	expiryDate: string;
+	location: string;
+	description: string;
+	studentifyAccountId: number;
+}
+
+interface EventListProps {
+  events: Event[];
+}
+
+const EventList: React.FC<EventListProps> = ({ events }) => {
+  const eventItems = events.map(event => (
+    <EventItem key={event.id}>
+      <EventHeader>
+        {event.name}
+        <EventDate>{event.creationDate.substring(0, 10)}</EventDate>
+      </EventHeader>
+      <p>{event.description}</p>
+    </EventItem>
+  ))
   return (
-    <ul>
-      <li>Event 1</li>
-      <li>Event 2</li>
-      <li>Event 3</li>
-    </ul>
+    <>
+    	<EventsHeader>
+				<h1>Current Events:</h1>
+				<Button 
+					endIcon={<FilterListIcon/>} 
+					variant="contained" 
+					size="small" 
+					color="primary"
+				>
+					filters
+				</Button>
+			</EventsHeader>
+      <List>
+        {eventItems}
+      </List>
+    </>
   );
 }
 
