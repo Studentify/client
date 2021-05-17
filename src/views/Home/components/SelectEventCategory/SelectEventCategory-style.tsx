7a4f42560790078f191287afe070d01b7ddf8b14
Adrian Furman <fadikk367@gmail.com>
2021-05-17 17:18:48 +0200

    Implement SelectEventCategory component

---
+++ src/views/Home/components/SelectEventCategory/SelectEventCategory-style.tsx
@@ -0,0 +1,50 @@
+import styled from 'styled-components';
+import { Typography, Card, CardActionArea } from '@material-ui/core';
+
+
+export const StepWrapper = styled.div`
+  grid-column: 1/-1;
+
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`;
+
+export const Tiles = styled.div`
+  flex-grow: 1;
+  display: flex;
+  justify-content: center;
+  gap: 2rem;
+  height: 200px;
+  position: relative;
+`;
+
+export const OptionCard = styled(Card)`
+  flex: 1;
+
+`;
+
+export const OptionContent = styled(CardActionArea)<{ isSelected: boolean }>`
+  height: 100%;
+
+  && {
+    background-color: ${props => props.isSelected ? "#3edb87" : "white"};
+    transition: background-color 0.2s ease-in-out;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+export const ErrorMessage = styled(Typography)`
+  && {
+    height: 40px;
+    line-height: 40px;
+  }
+`;
+
+export const Controls = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
--- src/views/Home/components/index.ts
+++ src/views/Home/components/index.ts
@@ -2,3 +2,4 @@ export { default as EventList } from "./EventList";
 export { default as EventMap } from "./EventMap";
 export { default as EventsFilterForm} from "./EventsFilterForm";
 export { default as AddEventForm} from "./AddEventForm";
+export { default as SelectEventCategory} from "./SelectEventCategory";
