39eeb662fccf8627c47deee82b8eee47e5ce85dc
Adrian Furman <fadikk367@gmail.com>
2021-06-12 20:27:07 +0200

    Improve MessagesView structure and make use of new components

--- src/views/Messages/Messages-style.tsx
+++ src/views/Messages/Messages-style.tsx
@@ -3,5 +3,12 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   min-height: calc(100vh - 70px);
+  padding: 20px 40px;
   margin-top: 70px;
+`;
+
+export const ThreadsList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
 `;
--- src/views/Messages/Messages.tsx
+++ src/views/Messages/Messages.tsx
@@ -1,29 +1,30 @@
 import React from 'react';
 
 import { Wrapper } from './Messages-style';
+import { ThreadsList } from './components';
 
 import { MessagesProps, Message } from './types';
+import { isDateToday } from 'utils/date';
 
 const messages: Message[] = [
-  { id: 0, date: new Date().toISOString(), content: "Lorem ipsum sit dolor amet", threadId: 1},
-  { id: 1, date: new Date().toISOString(), content: "Lorem ipsum sit dolor amet", threadId: 1},
-  { id: 2, date: new Date().toISOString(), content: "Lorem ipsum sit dolor amet", threadId: 1},
-  { id: 3, date: new Date().toISOString(), content: "Lorem ipsum sit dolor amet", threadId: 1},
-  { id: 4, date: new Date().toISOString(), content: "Lorem ipsum sit dolor amet", threadId: 2},
-  { id: 5, date: new Date().toISOString(), content: "Lorem ipsum sit dolor amet", threadId: 2},
-  { id: 6, date: new Date().toISOString(), content: "Lorem ipsum sit dolor amet", threadId: 2},
+  { id: 0, date: new Date().toISOString(), content: "Lorem ipsum sit dolor amet", threadId: 1, author: { firstName: "John", lastName: "Doe" }},
+  { id: 1, date: new Date().toISOString(), content: "Lorem ipsum sit dolor amet", threadId: 1,  author: { firstName: "John", lastName: "Doe" }},
+  { id: 2, date: new Date(2021, 1, 20).toISOString(), content: "Lorem ipsum sit dolor amet", threadId: 1,  author: { firstName: "John", lastName: "Doe" }},
+  { id: 3, date: new Date(2021, 1, 21).toISOString(), content: "Lorem ipsum sit dolor amet", threadId: 1,  author: { firstName: "John", lastName: "Doe" }},
+  { id: 4, date: new Date(2021, 2, 20).toISOString(), content: "Lorem ipsum sit dolor amet", threadId: 2,  author: { firstName: "John", lastName: "Doe" }},
+  { id: 5, date: new Date(2021, 1, 21).toISOString(), content: "Lorem ipsum sit dolor amet", threadId: 2,  author: { firstName: "John", lastName: "Doe" }},
+  { id: 6, date: new Date(2021, 3, 20).toISOString(), content: "Lorem ipsum sit dolor amet", threadId: 2,  author: { firstName: "John", lastName: "Doe" }},
 ]
 
 
 const Messages: React.FC<MessagesProps> = () => {
+  const todayMessages = messages.filter(message => isDateToday(message.date));
+
+  const messagesFromPast = messages.filter(message => todayMessages.indexOf(message) === -1)
   return (
     <Wrapper>
-      <h3>Your messages</h3>
-      <ul>
-        <li>
-          <ul></ul>
-        </li>
-      </ul>
+      <ThreadsList title="Todays messages:" threads={todayMessages}/>
+      <ThreadsList title="History:" threads={messagesFromPast}/>
     </Wrapper>
   )
 }
