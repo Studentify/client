073d765e302f1224d73489ffb97d40a16e735214
Adrian Furman <fadikk367@gmail.com>
2021-06-13 19:09:21 +0200

    Finish EventInfo component

--- src/views/ThreadMessages/components/EventInfo/EventInfo-style.tsx
+++ src/views/ThreadMessages/components/EventInfo/EventInfo-style.tsx
@@ -1,8 +1,39 @@
+import { Typography } from '@material-ui/core';
 import styled from 'styled-components';
 
 
 export const Container = styled.div`
-  flex: 1;
-  padding: 1rem;
-  background-color: #e5e6b2;
+  flex: 3;
+  padding: 0.5rem;
+
+  padding-left: 2rem;
+  border-left: 2px solid #d4d4d4;
+`;
+
+export const EventHeader = styled(Typography)`
+  && {
+    display: flex;
+    justify-content: space-between;
+  }
+`;
+
+export const EventMeta = styled(Typography)`
+  && {
+    display: flex;
+    align-items: center;
+    gap: 0.3rem;
+  }
+`;
+
+export const EventDate = styled(Typography)`
+  && {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 0.3rem;
+
+    color: gray;
+    font-size: 0.8rem;
+    font-weight: 300;
+  }
 `;
