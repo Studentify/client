d25236e867e73b003b80a8e09045c3434cf8a0cf
Adrian Furman <fadikk367@gmail.com>
2021-06-14 00:00:35 +0200

    Add messages sending feature to messages list

--- src/views/ConversationThreads/ConversationThreads.tsx
+++ src/views/ConversationThreads/ConversationThreads.tsx
@@ -4,44 +4,34 @@ import axios from 'api/axiosInstance';
 import { Wrapper } from './ConversationThreads-style';
 import { ThreadsList } from './components';
 
-import { MessagesThread } from './types';
+import { ConversationThread } from './types';
 import { isDateToday } from 'utils/date';
-
-const messages: MessagesThread[] = [
-  { id: 0, date: new Date().toISOString(), content: "Lorem ipsum sit dolor amet", threadId: 1, author: { firstName: "John", lastName: "Doe" }},
-  { id: 1, date: new Date().toISOString(), content: "Lorem ipsum sit dolor amet", threadId: 1,  author: { firstName: "John", lastName: "Doe" }},
-  { id: 2, date: new Date(2021, 1, 20).toISOString(), content: "Lorem ipsum sit dolor amet", threadId: 1,  author: { firstName: "John", lastName: "Doe" }},
-  { id: 3, date: new Date(2021, 1, 21).toISOString(), content: "Lorem ipsum sit dolor amet", threadId: 1,  author: { firstName: "John", lastName: "Doe" }},
-  { id: 4, date: new Date(2021, 2, 20).toISOString(), content: "Lorem ipsum sit dolor amet", threadId: 2,  author: { firstName: "John", lastName: "Doe" }},
-  { id: 5, date: new Date(2021, 1, 21).toISOString(), content: "Lorem ipsum sit dolor amet", threadId: 2,  author: { firstName: "John", lastName: "Doe" }},
-  { id: 6, date: new Date(2021, 3, 20).toISOString(), content: "Lorem ipsum sit dolor amet", threadId: 2,  author: { firstName: "John", lastName: "Doe" }},
-]
 
 
 const MessagesView = () => {
-  const [messagesThreads, setMessagesThreads] = useState<MessagesThread[]>([]);
+  const [conversationThreads, setConversationThreads] = useState<ConversationThread[]>([]);
 
   useEffect(() => {
     fetchThreads();
 
     async function fetchThreads() {
       try {
-        const res = await axios.get<MessagesThread[]>('/Threads');
-        console.log(res.data);
+        const res = await axios.get<ConversationThread[]>('/Threads');
+        setConversationThreads(res.data);
 
       } catch(err) {
         console.log(err);
       }
     }
-  }, [setMessagesThreads])
+  }, [setConversationThreads])
 
-  const todayMessages = messages.filter(message => isDateToday(message.date));
-  const messagesFromPast = messages.filter(message => todayMessages.indexOf(message) === -1);
+  const threadsWIthTodaysMessages = conversationThreads.filter(thread => isDateToday(thread.lastMessage.date));
+  const remainingThreads = conversationThreads.filter(thread => threadsWIthTodaysMessages.indexOf(thread) === -1);
 
   return (
     <Wrapper>
-      <ThreadsList title="Todays messages:" threads={todayMessages}/>
-      <ThreadsList title="History:" threads={messagesFromPast}/>
+      <ThreadsList title="Todays messages:" threads={threadsWIthTodaysMessages}/>
+      <ThreadsList title="History:" threads={remainingThreads}/>
     </Wrapper>
   );
 }
